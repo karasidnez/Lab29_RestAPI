6451252b1daefed798c7adb0f49c918b2ec75a44
karasidnez <karasdnez@gmail.com>
2026-05-16 14:36:38 +0300

    Step 7: CORS configured in Program.cs

--- TaskApi/Program.cs
+++ TaskApi/Program.cs
@@ -3,12 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AlloAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+        
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    app.UseCors("AlowAll");
     app.UseSwaggerUI();
 }
 
